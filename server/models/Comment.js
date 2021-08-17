1f45dce929e9b0418209eeff7c0938f3035a154f
LAPTOP-9KDDCU7B\kkkuk <kkuk090@gmail.com>
2021-08-17 23:27:18 +0900

    replies Comment

---
+++ server/models/Comment.js
@@ -0,0 +1,31 @@
+// const { Schema } = require('mongoose');
+const { Number } = require('mongoose');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const commentSchema = mongoose.Schema({
+
+    writer: {
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    postId: {
+        type:Schema.Types.ObjectId,
+        ref:'Video'
+    },
+    responseTo: {
+        type: Schema.Types.ObjectId,
+        ref:'User'
+
+    },
+    content:{
+        String
+    } 
+
+}, { timestamps : true })
+
+
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = { Comment }
